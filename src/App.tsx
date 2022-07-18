d0c2ab348bdd2dd4ab19022faca31a8e2bf74ad4
AdriDevelopsThings <adrianbloober@gmail.com>
2022-07-18 21:33:58 +0200

    add shuffle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import languages from './languages';
 import quotes from './quotes';
 import spotifyIcon from './spotify.svg';
+import shuffleIcon from './shuffle.png';
 import * as ackee from 'ackee-tracker'
 
 
@@ -67,9 +68,37 @@ const Language = styled.li`
   cursor: pointer;
 `
 
-const getRandomQuote = (language: string | null) => {
-  const filteredQuotes = quotes.filter(quote => RegExp(language || '.*').test(quote.lang))
-  return filteredQuotes[Math.floor(Math.random() * filteredQuotes.length)]
+const Shuffle = styled.div`
+  position: fixed;
+  bottom: 3vh;
+  left: 3vw;
+  cursor: pointer;
+`
+
+const ShuffleIcon = styled.img`
+  max-width: 50%;
+  width: 25%;
+`
+
+const getAlreadyUsedQuotes = () => sessionStorage.getItem('quotes') ? JSON.parse(sessionStorage.getItem('quotes')!) : []
+
+const addAlreadyUsedQuote = (trackId: string) => {
+  const quotes = getAlreadyUsedQuotes()
+  if (!quotes.includes(trackId))
+    quotes.push(trackId)
+  sessionStorage.setItem('quotes', JSON.stringify(quotes))
+}
+
+const getRandomQuote = (language: string | null): { trackId: string, lang: string, quote: string } => {
+  const alreadyUsedQuotes = getAlreadyUsedQuotes()
+  const filteredQuotes = quotes.filter(quote => RegExp(language || '.*').test(quote.lang)).filter(quote => !alreadyUsedQuotes.includes(quote.trackId))
+  if (filteredQuotes.length === 0 ) {
+    sessionStorage.removeItem('quotes')
+    return getRandomQuote(language)
+  }
+  const quote = filteredQuotes[Math.floor(Math.random() * filteredQuotes.length)]
+  addAlreadyUsedQuote(quote.trackId)
+  return quote
 }
 
 const getQuoteById = (trackId: string) => {
@@ -84,16 +113,18 @@ function App() {
     ackee.create('https://ackee.adridoesthings.com', { ignoreLocalhost: true, ignoreOwnVisits: false, detailed: true }).record('f9cadca5-2fd4-4c3c-b0a6-d5310d6bada3')
   }
   const [ language, setLanguge ] = useState(sessionStorage.getItem('language') || '.*')
+  const [ quote, setQuote ] = useState({ trackId: '', lang: 'en', quote: ''})
 
-  let quote = null
+  useEffect(() => {
+    const pathname = window.location.pathname.slice(1).split('/')
+    if (pathname) {
+      setQuote(getQuoteById(pathname[pathname.length - 1])!)
+    } else {
+      setQuote(getRandomQuote(language))
+    } 
+  }, [])
 
-  const pathname = window.location.pathname.slice(1).split('/')
-  if (pathname) {
-    quote = getQuoteById(pathname[pathname.length - 1])  
-  }
-  if (!quote) {
-    quote = getRandomQuote(language)
-  }
+  useEffect(() => setQuote(getRandomQuote(language)), [language])
 
   return (
     <>
@@ -109,6 +140,9 @@ function App() {
         <Quote>{ quote.quote.split('\n').map((quoteStr, index) => <QuoteLine key={index}>{ quoteStr }</QuoteLine>)}</Quote>
         <SpotifyLink href={`https://open.spotify.com/track/${quote.trackId}`} target='_blank' rel='noreferrer'><SpotifyIcon src={spotifyIcon} alt='spotify icon'/></SpotifyLink>
       </AppStyle>
+      <Shuffle>
+        <ShuffleIcon src={shuffleIcon} onClick={e => setQuote(getRandomQuote(language))} />
+      </Shuffle>
     </>
   );
 }
